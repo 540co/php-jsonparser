87e420e65ab4f52b58e3504e8b572b33f4b65a33
jobrieniii <jobrieniii@yahoo.com>
2016-03-11 04:32:51 -0500

    removed fields from output

--- tests/newStructureExample.php
+++ tests/newStructureExample.php
@@ -8,10 +8,10 @@ use Keboola\Utils\Utils;
 date_default_timezone_set('America/Los_Angeles');
 
 $r = array();
-$r['foo'] = json_decode(file_get_contents('./tests/_data/simple/foo.json'));
-$r['bar'] = json_decode(file_get_contents('./tests/_data/simple/bar.json'));
-//$r['ff52436aa931128b4220047f67a956d2b3aab8eb'] = json_decode(file_get_contents('./tests/_data/lineitems/ff52436aa931128b4220047f67a956d2b3aab8eb.json'));
-//$r['ffa9b89f42e0e059df93cd3e79f328e1f11b359f'] = json_decode(file_get_contents('./tests/_data/lineitems/ffa9b89f42e0e059df93cd3e79f328e1f11b359f.json'));
+//$r['foo'] = json_decode(file_get_contents('./tests/_data/simple/foo.json'));
+//$r['bar'] = json_decode(file_get_contents('./tests/_data/simple/bar.json'));
+$r['ff52436aa931128b4220047f67a956d2b3aab8eb'] = json_decode(file_get_contents('./tests/_data/lineitems/ff52436aa931128b4220047f67a956d2b3aab8eb.json'));
+$r['ffa9b89f42e0e059df93cd3e79f328e1f11b359f'] = json_decode(file_get_contents('./tests/_data/lineitems/ffa9b89f42e0e059df93cd3e79f328e1f11b359f.json'));
 
 $parser = \FiveFortyCo\Json\Parser::create(new \Monolog\Logger('json-parser'));
 $parser->process($r);
